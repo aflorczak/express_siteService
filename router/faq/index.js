ab61c95cc65dbd1766c66c0c91e09798f66e8495
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-05-19 13:25:14 +0200

    Get FAQ data from other service.

--- router/faq/index.js
+++ router/faq/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 router.get('/faq', (req, res) => {
-    res.render('pages/faq/home', {});
+    axios.get(`http://localhost:8081/faq`)
+        .then((data) => {
+            const faqs = data.data.data;
+            res.render('pages/faq/home', {
+                'faqs': faqs
+            });
+        })
+        .catch((error) => {
+            const status = 500;
+            const message = error.cause.code || null;
+            res.render('errors/other', { status, message });
+        })
 });
 
 export default router;
