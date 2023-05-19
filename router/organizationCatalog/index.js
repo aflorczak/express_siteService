aba6525be71560a37ce71beb68821f4aa3162d68
Adrian Florczak <kontakt@adrianflorczak.pl>
2023-05-19 14:16:44 +0200

    Axios in organizationCatalog

--- router/organizationCatalog/index.js
+++ router/organizationCatalog/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import axios from "axios";
 
 const router = express.Router();
 
 router.get('/organizacje', (req, res) => {
-    res.render('pages/organizationCatalog/home', {});
+    axios.get(`http://localhost:8081/organizations`)
+        .then((data) => {
+            const organizations = data.data.data;
+            res.render('pages/organizationCatalog/home', {
+                'organizations': organizations
+            });
+        })
+        .catch((error) => {
+            const status = 500;
+            const message = error.cause.code || null;
+            res.render('errors/other', { status, message });
+        })
 });
 
 export default router;
